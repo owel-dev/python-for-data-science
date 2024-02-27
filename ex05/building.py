32cbc552c528f04f03168d832f51931d787231da
owel-dev <krkr728@gmail.com>
2024-02-27 23:45:06 +0900

    fix: ex05 flake8 규칙 준수 및 .idea gitignore 추가

--- ex05/building.py
+++ ex05/building.py
@@ -1,12 +1,14 @@
 import sys
 
+
 def main():
     """
     문자열을 받아 문자열을 구성하는 문자들의 속성별 갯수를 출력합니다.
 
     Example:
-    $> python building.py "Python 3.0, released in 2008, 
-    was a major revision that is not completely backwardcompatible with earlier versions. 
+    $> python building.py "Python 3.0, released in 2008,
+    was a major revision that is not completely backwardcompatible with
+    earlier versions.
     Python 2 was discontinued with version 2.7.18 in 2020."
     The text contains 171 characters:
     2 upper letters
@@ -17,13 +19,11 @@ def main():
     """
     if len(sys.argv) == 1:
         return
-    
     try:
         assert len(sys.argv) == 2, 'more than one argument is provided'
     except AssertionError as error:
         print(error)
-        return 
-    
+        return
     string = sys.argv[1]
     upper_count = 0
     lower_count = 0
@@ -42,12 +42,12 @@ def main():
             digit_count += 1
         else:
             punct_count += 1
-    
     print(f'{upper_count} upper letters')
     print(f'{lower_count} lower letters')
     print(f'{punct_count} punctuation marks')
     print(f'{space_count} spaces')
     print(f'{digit_count} digits')
+
 
 if __name__ == "__main__":
     main()
