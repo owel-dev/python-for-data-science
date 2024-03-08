1169738019c9ac59d75d3d0992cced73421f1e4b
owel-dev <krkr728@gmail.com>
2024-03-08 23:51:25 +0900

    feat: ex07

---
+++ ex07/sos.py
@@ -0,0 +1,58 @@
+import sys
+
+NESTED_MORSE = {
+    " ": "/ ",
+    "A": ".- ",
+    "B": "-... ",
+    "C": "-.-. ",
+    "D": "-.. ",
+    "E": ". ",
+    "F": "..-. ",
+    "G": "--. ",
+    "H": ".... ",
+    "I": ".. ",
+    "J": ".--- ",
+    "K": "-.- ",
+    "L": ".-.. ",
+    "M": "-- ",
+    "N": "-. ",
+    "O": "--- ",
+    "P": ".--. ",
+    "Q": "--.- ",
+    "R": ".-. ",
+    "S": "... ",
+    "T": "- ",
+    "U": "..- ",
+    "V": "...- ",
+    "W": ".-- ",
+    "X": "-..- ",
+    "Y": "-.-- ",
+    "Z": "--.. ",
+    "0": "----- ",
+    "1": ".---- ",
+    "2": "..--- ",
+    "3": "...-- ",
+    "4": "....- ",
+    "5": "..... ",
+    "6": "-.... ",
+    "7": "--... ",
+    "8": "---.. ",
+    "9": "----. "
+}
+
+def sos():
+    ret = ""
+    try:
+        assert len(sys.argv) == 2, "the arguments are bad"
+        string = sys.argv[1]
+        for char in string:
+            upper = char.upper()
+            assert upper.isalnum() or upper.isspace(), "the arguments are bad"
+            morse = NESTED_MORSE[upper]
+            ret += morse
+        print(ret)
+    except AssertionError as error:
+        print(error)
+
+if __name__ == "__main__":
+    sos()
